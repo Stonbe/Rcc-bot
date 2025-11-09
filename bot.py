8c94925b7857552d7e32ea3bab10bb880ae83f9e
Stonbe <159662078+Stonbe@users.noreply.github.com>
2025-11-09 18:51:19 +0200

    Host Name Add

--- bot.py
+++ bot.py
@@ -10,11 +10,11 @@ intents.reactions = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-GUILD_ID = 1434590759570378774
+GUILD_ID = 1434590759570378774  # replace with your server ID
 GUILD = discord.Object(id=GUILD_ID)
 
-IMAGE_URL = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/73256248-616c-4779-aa68-106c298d0710/dhkcje3-3cd29923-0668-4bec-9e12-a769ef2be017.jpg/v1/fill/w_1151,h_694,q_70,strp/cars_tvg_boxart_roblox_remake_by_redkirbdaredpuffball_dhkcje3-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9Nzg0IiwicGF0aCI6Ii9mLzczMjU2MjQ4LTYxNmMtNDc3OS1hYTY4LTEwNmMyOThkMDcxMC9kaGtjamUzLTNjZDI5OTIzLTA2NjgtNGJlYy05ZTEyLWE3NjllZjJiZTAxNy5qcGciLCJ3aWR0aCI6Ijw9MTI5OSJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.hDvTXzd7zXPfY1NMj3QAIHT-LML9VDX9CbX98xCETTU"
-ROLE_ID = 1434592997869228052
+IMAGE_URL = "https://i.ytimg.com/vi/vIDAXZ2HT38/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLCqSI_HuzVFSS0EpodH2a_U0s7Tqwwhol"
+ROLE_ID = 1434592997869228052  # replace with the ID of the role allowed to run the command
 
 class SessionView(ui.View):
     def __init__(self, max_reactions: int, link: str):
@@ -22,19 +22,19 @@ class SessionView(ui.View):
         self.max_reactions = max_reactions
         self.link = link
         self.current_reactions = 0
-        self.users = set()  # track who reacted
+        self.reacted_users = set()
 
-        self.react_button = ui.Button(label=f"React (0/{self.max_reactions})", style=ButtonStyle.primary)
+        self.react_button = ui.Button(label="React (0/0)", style=ButtonStyle.primary)
         self.react_button.callback = self.react_callback
         self.add_item(self.react_button)
 
     async def react_callback(self, interaction: discord.Interaction):
         user_id = interaction.user.id
-        if user_id in self.users:
-            self.users.remove(user_id)
+        if user_id in self.reacted_users:
+            self.reacted_users.remove(user_id)
             self.current_reactions -= 1
         else:
-            self.users.add(user_id)
+            self.reacted_users.add(user_id)
             self.current_reactions += 1
 
         self.react_button.label = f"React ({self.current_reactions}/{self.max_reactions})"
@@ -77,10 +77,11 @@ async def session(interaction: discord.Interaction, channel: discord.TextChannel
     parsed_url = urlparse(link)
     title = parsed_url.netloc.replace("www.", "").capitalize() or "Session"
 
-    embed = discord.Embed(title=title, description="React to join!", color=discord.Color.blue())
-    embed.set_image(url=IMAGE_URL)
+    embed = discord.Embed(title=title, color=discord.Color.blue())
+    embed.add_field(name="Host", value=interaction.user.mention, inline=False)
     embed.add_field(name="Location", value=location, inline=True)
     embed.add_field(name="Theme", value=theme, inline=True)
+    embed.set_image(url=IMAGE_URL)
 
     view = SessionView(max_reactions=reactions, link=link)
     await channel.send(embed=embed, view=view)
@@ -93,13 +94,8 @@ async def session_error(interaction: discord.Interaction, error):
 
 @bot.event
 async def on_ready():
-    try:
-        synced = await bot.tree.sync(guild=GUILD)
-        print(f'Synced {len(synced)} commands.')
-    except Exception as e:
-        print('Failed to sync commands:', e)
+    await bot.tree.sync(guild=GUILD)
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
     print('Slash commands synced!')
 
 bot.run(os.environ['BOT_TOKEN'])
-
